788b3710ec30e6573881cf6633235e0cbb6d1ca9
erikwaahlstrom <erik93_93@hotmail.com>
2017-10-12 11:54:14 +0200

    upload

---
+++ src/Config/config.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG = {
+    apiKey: "AIzaSyD8wdjEkk5NV7NCXRekX3n7VSZGasM3xtc",
+    authDomain: "react-notes-13687.firebaseapp.com",
+    databaseURL: "https://react-notes-13687.firebaseio.com",
+    projectId: "react-notes-13687",
+    storageBucket: "react-notes-13687.appspot.com",
+    messagingSenderId: "44225409481"
+  };
